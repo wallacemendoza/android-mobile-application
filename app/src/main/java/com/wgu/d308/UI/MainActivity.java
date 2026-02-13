7570e420375bf737657f490f74bd722a4d22cd06
wmendo8 <wmendo8@wgu.edu>
2026-02-12 23:21:26 -0500

    Fix - Final changes

--- app/src/main/java/com/wgu/d308/UI/MainActivity.java
+++ app/src/main/java/com/wgu/d308/UI/MainActivity.java
@@ -1,8 +1,13 @@
 package com.wgu.d308.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,12 +17,18 @@ import com.wgu.d308.R;
 public class MainActivity extends AppCompatActivity {
 
     public static int numAlert;
-    public static int numAlert2;
+    private static final int REQUEST_CODE_POST_NOTIFICATIONS = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, REQUEST_CODE_POST_NOTIFICATIONS);
+            }
+        }
 
         Button button = findViewById(R.id.button);
 
