f0cf2ebb0b3742da025bb89ce64e41b891d02482
wmendo8 <wmendo8@wgu.edu>
2026-02-12 23:16:58 -0500

    B3 - Add vacation alerts (start and end) using AlarmManager + receiver

--- app/src/main/java/com/wgu/d308/UI/VacationAdapter.java
+++ app/src/main/java/com/wgu/d308/UI/VacationAdapter.java
@@ -10,7 +10,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.wgu.d308.Database.Repository;
 import com.wgu.d308.R;
+import com.wgu.d308.entities.Excursion;
 import com.wgu.d308.entities.Vacation;
 
 import java.util.List;
@@ -19,14 +21,15 @@ public class VacationAdapter extends RecyclerView.Adapter<VacationAdapter.Vacati
     private List<Vacation> mVacations;
     private final Context context;
     private final LayoutInflater mInflater;
+    private final Repository repository;
 
     public VacationAdapter(Context context) {
-        mInflater= LayoutInflater.from(context);
+        mInflater = LayoutInflater.from(context);
         this.context = context;
+        this.repository = new Repository(((VacationList) context).getApplication());
     }
 
     public class VacationViewHolder extends RecyclerView.ViewHolder {
-
         private final TextView vacationItemView;
         private final TextView hotelName;
         private final TextView vacationPrice;
@@ -35,7 +38,7 @@ public class VacationAdapter extends RecyclerView.Adapter<VacationAdapter.Vacati
 
         public VacationViewHolder(@NonNull View itemView) {
             super(itemView);
-            vacationItemView=itemView.findViewById(R.id.vacationTitle);
+            vacationItemView = itemView.findViewById(R.id.vacationTitle);
             hotelName = itemView.findViewById(R.id.hotelName);
             vacationPrice = itemView.findViewById(R.id.vacationPrice);
             startDateValue = itemView.findViewById(R.id.startDateValue);
@@ -43,8 +46,8 @@ public class VacationAdapter extends RecyclerView.Adapter<VacationAdapter.Vacati
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    int position=getAdapterPosition();
-                    final Vacation current=mVacations.get(position);
+                    int position = getAdapterPosition();
+                    final Vacation current = mVacations.get(position);
                     Intent intent = new Intent(context, VacationDetails.class);
                     intent.putExtra("id", current.getVacationID());
                     intent.putExtra("name", current.getVacationName());
@@ -57,38 +60,42 @@ public class VacationAdapter extends RecyclerView.Adapter<VacationAdapter.Vacati
             });
         }
     }
+
     @NonNull
     @Override
     public VacationAdapter.VacationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View itemView=mInflater.inflate(R.layout.vacation_list_item,parent,false);
+        View itemView = mInflater.inflate(R.layout.vacation_list_item, parent, false);
         return new VacationViewHolder(itemView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull VacationAdapter.VacationViewHolder holder, int position) {
-        if(mVacations!=null){
-            Vacation current=mVacations.get(position);
-            String name= current.getVacationName();
-            holder.vacationItemView.setText(name);
-            holder.hotelName.setText(current.getHotel());
-            holder.vacationPrice.setText(String.format("$%.2f", current.getPrice()));
-            holder.startDateValue.setText(current.getStartVacationDate());
-            holder.endDateValue.setText(current.getEndVacationDate());
-        }
-        else{
+        if (mVacations != null) {
+            Vacation currentVacation = mVacations.get(position);
+            double totalExcursionPrice = 0.0;
+            for (Excursion excursion : repository.getAllExcursions()) {
+                if (excursion.getVacationID() == currentVacation.getVacationID()) {
+                    totalExcursionPrice += excursion.getPrice();
+                }
+            }
+            double totalPrice = currentVacation.getPrice() + totalExcursionPrice;
+            holder.vacationItemView.setText(currentVacation.getVacationName());
+            holder.hotelName.setText(currentVacation.getHotel());
+            holder.vacationPrice.setText(String.format("$%.2f", totalPrice));
+            holder.startDateValue.setText(currentVacation.getStartVacationDate());
+            holder.endDateValue.setText(currentVacation.getEndVacationDate());
+        } else {
             holder.vacationItemView.setText("No vacation name");
         }
     }
 
     @Override
     public int getItemCount() {
-        if(mVacations != null) {
-            return mVacations.size();
-        }
-        else return 0;
+        return mVacations != null ? mVacations.size() : 0;
     }
-    public void setVacations(List<Vacation> vacations){
-        mVacations=vacations;
+
+    public void setVacations(List<Vacation> vacations) {
+        mVacations = vacations;
         notifyDataSetChanged();
     }
 }
